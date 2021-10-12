2a6135c4882c63bb802e363123e625e8ba4c9f49
kaiyang <gamedevleo@gmail.com>
2021-10-12 17:59:35 +1100

    Kaiyang-004:Finished homepage and animation

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Background Animation
 const bgEl = document.querySelector(".lm-animated-bg");
 
 document.addEventListener('mousemove',e=>{
@@ -6,3 +7,17 @@ document.addEventListener('mousemove',e=>{
 
   bgEl.style.setProperty("background-position","calc( 50% + " + x*-0.005 + "px ) calc( 50% + " + y*-0.005 + "px )");
 })
+
+
+const subtitle1El = document.querySelector(".subtitle1");
+const subtitle2El =document.querySelector(".subtitle2");
+
+setInterval(() => {
+  subtitle1El.classList.toggle("animate__animated");
+  subtitle1El.classList.add("display-none")
+  
+  setTimeout(() => {
+    subtitle2El.classList.toggle("animate__animated");
+  }, 3000);
+}, 3000);
+
